b6034ed0ea1e8675061bcf6637846080ed292e13
rscohen <rudy.s.cohen@gmail.com>
2019-01-04 14:02:07 +0100

    ajout question deploiement

--- script/exec.py
+++ script/exec.py
@@ -1,8 +1,44 @@
 # Import
 import os
+import sys
 
 # Tips : os.system('ls -al') si on veut exec la cmd
 # Tips : os.popen('ls -al') si on veut recup la sortie
+
+# Functions
+## Function query_yes_no
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+## End : Function query_yes_no
 
 # Partie graphique
 # -> Choix du nom du token
@@ -25,7 +61,7 @@ while type(token_Symbol) != type('string') or len(token_Symbol) < 1 :
     try :
         token_Symbol = eval(raw_input("Token symbol :"))
     # -> Verification des inputs :
-    #       - symbole == 3 lettres (a verifier)
+    #       - symbole == 3 lettres (a verifier)okoko
     except :
         print("You have to write a string with the apostrophe (ex : 'Token_Symbol')")
 
@@ -43,7 +79,13 @@ os.system("sed -i '' 's/input_supply/"+token_Supply+"/g' test.sol")
 
 # -> Fenetre de validation de deploiement du SC
 print("Thanks, your token is named : " + token_Name + "\n it symbol is : " + token_Symbol + "\n it supply is : " + token_Supply )
+
 # Partie executable
+answer = query_yes_no("Smart Contract's deployment")
+if answer == True :
+    os.system("truffle migrate --ropsten")
+else :
+    sys.exit()
 #  bash cmd : truffle migrate --ropsten
 #
 
