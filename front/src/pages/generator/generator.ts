172f2b6e8ca0f0085a8a606f9a58b2f170b52c0d
bastienchevallier <bchevallier57@gmail.com>
2019-01-05 09:59:01 +0100

    add checkInputs()

--- front/src/pages/generator/generator.ts
+++ front/src/pages/generator/generator.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams, NavController } from 'ionic-angular';
-import { Observable } from 'rxjs/Observable'
+
 
 @Component({
   templateUrl: 'generator.html'
@@ -11,18 +11,38 @@ export class GeneratorPage {
   constructor(public navParams: NavParams, public navCtrl: NavController) {
   }
 
-  checkSymbol(){
-
+  checkInputs(){
+    var x;
+    if(tokenName.value == ""){
+      alert("Token Name Field is missing");
+    } 
+    else if(tokenSymbol.value == ""){
+      alert("Token Symbol Field is missing");
+    }
+    else {
+      x = document.getElementById("tokenSupply").value
+      console.log("x");
+      console.log(x);
+      if (x == ""){
+        alert("Token Supply Field is missing");
+      }
+    }
   }
 
-  deployOnRopsten(){
-    
+  personalizeSmartContract(){
+    const phrase = 'I love my dog! Dogs are great'
+    const stripped = phrase.replace(/dog/g, '')
+  }
+
+  deployOnRopsten(){    
   }
 
   deployContract(){
   	//Check each input
+    this.checkInputs();
 
   	//Personalize the SmartContract according to the inputs
+    
 
   	//Deploy the SmartContract on the chosen Blockchain
     this.deployOnRopsten();
