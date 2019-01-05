e234144168df559a7367c929b67185453c53992a
bastienchevallier <bchevallier57@gmail.com>
2019-01-05 14:19:47 +0100

    fixed acces to input variables

--- front/src/pages/generator/generator.ts
+++ front/src/pages/generator/generator.ts
@@ -8,44 +8,61 @@ import { NavParams, NavController } from 'ionic-angular';
 
 export class GeneratorPage {
 
+  tokenName:string='';
+  tokenSymbol:string='';
+  tokenSupply:number= undefined;
+
   constructor(public navParams: NavParams, public navCtrl: NavController) {
   }
 
   checkInputs(){
-    var x;
-    if(tokenName.value == ""){
+    console.log(this.tokenName);
+    console.log(this.tokenName);
+    console.log(this.tokenName);
+    if(this.tokenName == ''){
       alert("Token Name Field is missing");
-    } 
-    else if(tokenSymbol.value == ""){
+    }
+    else if(this.tokenSymbol == ''){
       alert("Token Symbol Field is missing");
     }
-    else {
-      x = document.getElementById("tokenSupply").value
-      console.log("x");
-      console.log(x);
-      if (x == ""){
-        alert("Token Supply Field is missing");
-      }
+    else if(this.tokenSupply == undefined){
+      alert("Token Supply Field is missing");
+    }
+    else{
+      return true;
     }
   }
 
-  personalizeSmartContract(){
-    const phrase = 'I love my dog! Dogs are great'
-    const stripped = phrase.replace(/dog/g, '')
+  personalizeSmartContract(SmartContractPath){
+    var pathToSmartContract = '../../../../contracts/SimpleTokenTemplate.sol';
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var text = reader.result;
+    }
+    reader.readAsText(pathToSmartContract);
+    console.log(reader.result)
+
+    //var result = data.replace(/tName/g, this.tokenName);
+    //result = result.replace(/tSymbol/g, this.tokenSymbol);
+    //result = result.replace(/tSupply/g, this.tokenSupply);
+
   }
 
-  deployOnRopsten(){    
+  deployOnRopsten(){
   }
 
   deployContract(){
   	//Check each input
-    this.checkInputs();
+    var checked = this.checkInputs();
 
   	//Personalize the SmartContract according to the inputs
-    
+    if (checked == true){
+      var SmartContractPath = '../../../../contracts/PersonalizedSmartContract.sol'
+      this.personalizeSmartContract(SmartContractPath);
+    }
 
   	//Deploy the SmartContract on the chosen Blockchain
     this.deployOnRopsten();
   }
-  
+
 }
