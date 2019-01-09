307b8bcb501474b97cd3fd2a5d4ffe7ec5128942
rscohen <rudy.s.cohen@gmail.com>
2019-01-09 22:25:09 +0100

    deployment

--- node.js/controllers/DeploymentController.js
+++ node.js/controllers/DeploymentController.js
@@ -9,6 +9,7 @@ const sh = (cmd) => {
 
 const saveContract = () => {
   const fs = require('fs');
+  console.log("zeub");
   fs.writeFile('../contracts/Output.sol', contract, (err) => {
     // In case of a error throw err.
     if (err) throw err;
