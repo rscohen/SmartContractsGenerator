01c2424b6994f6c985c5238653813d1e2075760a
rscohen <rudy.s.cohen@gmail.com>
2019-01-13 17:24:09 +0100

    web.config

--- node.js/contracts/Output.sol
+++ node.js/contracts/Output.sol
@@ -16,7 +16,7 @@ contract Output is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed('Test', 'CNN', 18) {
+    constructor () public ERC20Detailed('CNN', 'CNN', 18) {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
