8b48ddcb08ee27727f7781d8c8c5b0311ebd0c1f
bastienchevallier <bchevallier57@gmail.com>
2019-02-03 00:19:55 -0800

    build contract without using truffle OK, need to add deployment

--- node.js/controllers/DeploymentController.js
+++ node.js/controllers/DeploymentController.js
@@ -1,4 +1,102 @@
+const solc = require('solc');
+const fs = require('fs');
+
 var contract = null;
+
+// Input parameters for solc
+// Refer to https://solidity.readthedocs.io/en/develop/using-the-compiler.html#compiler-input-and-output-json-description
+var solcInput = {
+    language: "Solidity",
+    sources: { },
+    settings: {
+        optimizer: {
+            enabled: true
+        },
+        evmVersion: "byzantium",
+        outputSelection: {
+            "*": {
+              "": [
+                "legacyAST",
+                "ast"
+              ],
+              "*": [
+                "abi",
+                "evm.bytecode.object",
+                "evm.bytecode.sourceMap",
+                "evm.deployedBytecode.object",
+                "evm.deployedBytecode.sourceMap",
+                "evm.gasEstimates"
+              ]
+            },
+        }
+    }
+};
+
+// Try to lookup imported sol files in "contracts" folder or "node_modules" folder
+const findImports = (importFile) => {
+    console.log("Import File:" + importFile);
+
+    try {
+        // Find in contracts folder first
+        var result = fs.readFileSync(__dirname + "/../contracts/" + importFile, 'utf8');
+        return { contents: result };
+    } catch (error) {
+        // Try to look into node_modules
+        try {
+            var result = fs.readFileSync(__dirname + "/../node_modules/" + importFile, 'utf8');
+            return { contents: result };
+        } catch (error) {
+            console.log(error.message);
+            return { error: 'File not found' };
+        }
+    }
+
+}
+
+// Compile the sol file in "contracts" folder and output the built json file to "build/contracts"
+const buildContract = (contractname) => {
+    let jsonOutputName = 'Output' + '.json';
+    let jsonOutputFile = __dirname + '/../build/contracts/' + jsonOutputName;
+
+    solcInput.sources[contractname] = {
+        "content": contract
+    };
+
+    let solcInputString = JSON.stringify(solcInput);
+    let output = solc.compileStandardWrapper(solcInputString, findImports);
+
+    let jsonOutput = JSON.parse(output);
+    let isError = false;
+
+    if (jsonOutput.errors) {
+        jsonOutput.errors.forEach(error => {
+            console.log(error.severity + ': ' + error.component + ': ' + error.formattedMessage);
+            if (error.severity == 'error') {
+                isError = true;
+            }
+        });
+    }
+
+    if (isError) {
+        // Compilation errors
+        console.log('Compile error!');
+        return false;
+    }
+
+    // Update the sol file checksum
+    //jsonOutput['contracts'][contract]['checksum'] = contractFileChecksum;
+
+    let formattedJson = JSON.stringify(jsonOutput, null, 4);
+
+    // Write the output JSON
+    fs.writeFileSync(jsonOutputFile, formattedJson);
+
+    console.log('==============================');
+
+    return true;
+}
+
+//%%%%%%%%%%%%%%%%% OLD CODE %%%%%%%%%%%%%%
 
 const sh = (cmd) => {
   const exec = require('child_process').exec
@@ -8,7 +106,6 @@ const sh = (cmd) => {
 }
 
 const saveContract = () => {
-  const fs = require('fs');
   fs.writeFile(__dirname + '/../contracts/Output.sol', contract, (err) => {
     // In case of a error throw err.
     if (err) throw err;
@@ -29,8 +126,11 @@ export default {
 
   deploy: (req, res) => {
     var network = req.body.network;
+    //Save contract.sol
     saveContract();
-    sh('sudo truffle build && sudo truffle migrate --network ropsten')
+    //Build contract.json
+    let result = buildContract('Output.sol');
+    //sh('sudo truffle build && sudo truffle migrate --network ropsten');
     res.render('final')
   },
 }
